882728a383eef48c6e6ca970a9c4085958691d9a
Shani Tiwari <shanitiwari3354@gmail.com>
2025-08-07 08:20:15 +0530

    Create Switch.dart

---
+++ Switch.dart
@@ -0,0 +1,29 @@
+void main() {
+  int day = 3;
+  switch(day) {
+    case 1:
+      print("Mon");
+      break;
+    case 2:
+      print("Tue");
+      break;
+    case 3:
+      print("Wed");
+      break;
+    case 4:
+      print("Thu");
+      break;
+    case 5:
+      print("Fri");
+      break;
+    case 6:
+      print("Sat");
+      break;
+    case 7:
+      print("Sun");
+      break;
+    default:
+      print("Invalid");
+      break;
+  }
+}
